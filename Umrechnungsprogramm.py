a93fe650822b94e0089e01e587bc7b164ee6f478
Timnjr <timlucaneujahr@icloud.com>
2024-08-20 16:07:11 +0200

    Version 3
    Das Menü wurde als Funktion gespeichert um es leichter wieder zu verwenden. Das Programm wurde mit dem while Befehl in eine Schleife eingebettet damit sich das Programm nicht nach der Eingabe automatisch beendet. Dazu habe ich noch eine Abbruchbedingung (0) hinzugefügt und nach der Nutzereingabe eine Abfrage ob das Menü nochmals aufgerufen oder das Programm beendet werden soll. Die Umrechnung von Grad Celsius in Fahrenheit war falsch dies wurde auch korrigiert.

--- Umrechnungsprogramm.py
+++ Umrechnungsprogramm.py
@@ -1,45 +1,76 @@
 #Begrüßung des Nutzers
 print("Willkommen im Umrechnungsprogramm!")
+
 #Benutzermenü
-print("Menüauswahl:")
-print("1: Bar in Pascal.")
-print("2: m/s in Km/h.")
-print("3: Kilometer in Meilen.")
-print("4: Celsius in Fahrenheit")
-print("5: Zentimeter in Zoll")
-eingabe = input("Welche Einheit darf ich für dich umrechnen?\n")
-#Umrechnung Bar in Pascal
-if eingabe == '1' :
-    print("Du möchtest Bar in Pascal umrechnen.")
-    bar = float(input("Gebe deinen Wert in Bar ein:"))
-    urf_bar_pa = 100000
-    pascal = bar * urf_bar_pa
-    print(bar, "Bar sind", pascal, "Pascal.")
-#Umrechnung m/s in Km/h
-if eingabe == '2' :
-    print("Du möchtest m/s in Km/h umrechnen.")
-    m_s = float(input("Gebe deinen Wert in m/s ein:"))
-    urf_ms_kmh = float(3.6)
-    km_h = m_s / urf_ms_kmh
-    print(m_s,"m/s sind", km_h,"Km/h.")
-#Umrechnung Kilometer in Meilen
-if eingabe == '3' :
-    print("Du möchtest Kilometer in Meilen umrechnen.")
-    km = float(input("Gebe deinen Wert in Km ein: "))
-    urf_km_mi = float(1.609)
-    mi = km / urf_km_mi
-    print(km,"Km sind", mi,"Meilen.")
-#Umrechnung Celsius in Fahrenheit
-if eingabe == '4' :
-    print("Du möchtest Celsius in Fahrenheit umrechnen.")
-    tc = float(input("Gebe deinen Wert in Grad Celsius ein: "))
-    tf = (tc + 1.8) + 32
-    print(tc, "Grad Celsius sind", tf,"Grad Fahrenheit.")
-#Umrechnung Zentimeter in Zoll
-if eingabe == '5' :
-    print("Du möchtest Zentimeter in Zoll umrechnen.")
-    cm = float(input("Gebe deinen Wert in cm ein: "))
-    urf_cm_zoll = float(2.54) 
-    zoll = cm / urf_cm_zoll
-    print(cm,"cm sind", zoll,"Zoll.")
+def menü():
+    print("Menüauswahl:")
+    print("[1] Bar zu Pascal.")
+    print("[2] Meter pro Sekunde zu Kilometer pro Stunde.")
+    print("[3] Kilometer zu Meilen.")
+    print("[4] Grad Celsius zu Grad Fahrenheit.")
+    print("[5] Zentimeter zu Zoll.")
+    print("[0] Beenden.")
+
+#Hauptschleife des Programms
+while True:
+    menü()
+    eingabe = int(input("Welche Einheit darf ich für dich umrechnen?\n"))
     
+    #Abbruchbedingung der Schleife
+    if eingabe == 0:
+        print("Ich hoffe, ich konnte dir helfen. Bis bald!")
+        break
+    
+    #Umrechnung Bar in Pascal
+    elif eingabe == 1:
+        print("Du möchtest Bar zu Pascal umrechnen.")
+        bar = float(input("Gib deinen Wert in Bar ein: "))
+        urf_bar_pa = 100000
+        pascal = bar * urf_bar_pa
+        print(bar, "Bar sind", pascal, "Pascal.")
+    
+    #Umrechnung Meter pro Sekunde in Kilometer pro Stunde
+    elif eingabe == 2:
+        print("Du möchtest Meter pro Sekunde in Kilometer pro Stunde umrechnen.")
+        m_s = float(input("Gib deinen Wert in m/s ein: "))
+        urf_ms_kmh = 3.6
+        km_h = m_s * urf_ms_kmh
+        print(m_s, "m/s sind", km_h, "km/h.")
+    
+    #Umrechnung Kilometer in Meilen
+    elif eingabe == 3:
+        print("Du möchtest Kilometer zu Meilen umrechnen.")
+        km = float(input("Gib deinen Wert in km ein: "))
+        urf_km_mi = 1.609
+        mi = km / urf_km_mi
+        print(km, "km sind", mi, "Meilen.")
+    
+    #Umrechnung Grad Celsius in Grad Fahrenheit
+    elif eingabe == 4:
+        print("Du möchtest Grad Celsius zu Grad Fahrenheit umrechnen.")
+        tc = float(input("Gib deinen Wert in Grad Celsius ein: "))
+        tf = (tc * 1.8) + 32
+        print(tc, "Grad Celsius sind", tf, "Grad Fahrenheit.")
+    
+    #Umrechnung Zentimeter in Zoll
+    elif eingabe == 5:
+        print("Du möchtest Zentimeter zu Zoll umrechnen.")
+        cm = float(input("Gib deinen Wert in cm ein: "))
+        urf_cm_zoll = 2.54
+        zoll = cm / urf_cm_zoll
+        print(cm, "cm sind", zoll, "Zoll.")
+    
+    #Antwort auf ungültige Option
+    else:
+        print("Ungültige Option!")
+    
+    #Abfrage, ob der Nutzer zurück ins Menü möchte.
+    while True:
+        back_to_menu = input("Möchtest du eine weitere Umrechnung durchführen? [J]/[N] \n").upper()
+        if back_to_menu == 'J':
+            break
+        elif back_to_menu == 'N':
+            print("Ich hoffe, ich konnte dir helfen. Bis bald!")
+            exit()
+        else:
+            print("Ungültige Option! Bitte gib 'J' oder 'N' ein.")
