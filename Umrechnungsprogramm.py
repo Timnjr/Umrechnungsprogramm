2b37b8e7ed1ce7aa2cdb20e7a0b8e08d76875e8a
Timnjr <timlucaneujahr@icloud.com>
2024-08-20 10:42:38 +0200

    Erste Version
    Hier wurde erstmal der Grundaufbau des Menüs erstellt.

---
+++ Umrechnungsprogramm.py
@@ -0,0 +1,13 @@
+#Begrüßung des Nutzers
+print("Willkommen im Umrechnungsprogramm!")
+#Benutzermenü
+print("Menüauswahl:")
+print("1: Bar in Pascal.")
+print("2: m/s in Km/h.")
+print("3: Kilometer in Meilen.")
+print("4: Celsius in Fahrenheit")
+print("5: Zentimeter in Zoll")
+eingabe = input("Welche Einheit darf ich für dich umrechnen?\n")
+
+
+    
